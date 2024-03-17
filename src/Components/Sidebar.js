f3e8dc66bbb4aa65a3bc9b7a763d1ee121c8c2c4
Aravind <aravindchakravarthi7@gmail.com>
2024-03-18 00:38:38 +0530

    first commit

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,128 @@
+import React, { useState } from 'react'
+import "../Css/Sidebar.css"
+import { SiAntdesign } from "react-icons/si";
+import { HiOutlineSpeakerphone } from "react-icons/hi";
+import { RiExpandLeftRightLine } from "react-icons/ri";
+import { TbSquarePlus } from "react-icons/tb";
+import { FaPlus } from "react-icons/fa6";
+import { MdOutlineFolder } from "react-icons/md";
+import { FaCaretDown } from "react-icons/fa6";
+import { MdOutlinePersonAddAlt } from "react-icons/md";
+import { TbHelp } from "react-icons/tb";
+
+
+const Sidebar = (props) => {
+    const [datas, setData] = useState(true);
+
+    function handleData(data) {
+        setData(data);
+        props.onData(data);
+    }
+
+
+	return (
+		<div className='sideconatiner'>
+			<div className='df jcsb shead'>
+				<div className='df'>
+					<div>
+						<img src='innovateHub.png' className='img1' />
+					</div>
+					<div className='headings'>
+						<h5 className='textlight'>INC</h5>
+						<div className='textdark'>InnovateHub</div>
+					</div>
+				</div>
+				<div>
+					<img src='img2.png' className='img2' />
+				</div>
+			</div>
+
+			<div className='menus1'>
+				<div className='df jcsb menu1'>
+					<div className='df'>
+						<SiAntdesign />
+						<div className='textdark'>Design team</div>
+					</div>
+					<div className='x1'>
+						x+1
+					</div>
+				</div>
+				<div className='df jcsb menu1'>
+					<div className='df'>
+						<HiOutlineSpeakerphone />
+						<div className='textdark'>Marketing Team</div>
+					</div>
+					<div className='x1'>
+						x+2
+					</div>
+				</div>
+				<div className='df jcsb menu1'>
+					<div className='df'>
+						<RiExpandLeftRightLine />
+						<div className='textdark'>Development team</div>
+					</div>
+					<div className='x1'>
+						x+3
+					</div>
+				</div>
+			</div>
+			<div className='menu1 df lastm'>
+				<TbSquarePlus />
+				<div>Create a team</div>
+			</div>
+
+			<div className='jcsb df folder'>
+			<div className='textlight '>FOLDERS</div>
+			<FaPlus className='textlight'/>
+			</div>
+
+			<div className='menu1 df jcsb' onClick={() => handleData(true)}>
+				<div className='df'><MdOutlineFolder />
+				<div className='textdark'>Products</div>
+				</div>
+				<div><FaCaretDown className='downsvg'/></div>
+
+			</div>
+			<div className='menu1 df jcsb' onClick={() => handleData(false)}>
+				<div className='df'><MdOutlineFolder />
+				<div className='textdark'>Sales</div>
+				</div>
+				<div><FaCaretDown className='downsvg'/></div>
+
+			</div>
+			<div className='menu1 df jcsb' onClick={() => handleData(false)}>
+				<div className='df'><MdOutlineFolder />
+				<div className='textdark'>Design</div>
+				</div>
+				<div><FaCaretDown className='downsvg'/></div>
+
+			</div>
+			<div className='menu1 df jcsb' onClick={() => handleData(false)}>
+				<div className='df'><MdOutlineFolder />
+				<div className='textdark'>Office</div>
+				</div>
+
+			</div>
+			<div className='menu1' onClick={() => handleData(false)}>
+				<div className='df'><MdOutlineFolder />
+				<div className='textdark'>Legal</div>
+				</div>
+
+			</div>
+			<div className='menu1 invite'>
+				<div className='df'>
+				< MdOutlinePersonAddAlt/>
+				<div className='textdark'>Invite teammates</div>
+				</div>
+			</div>
+			<div className='menu1'>
+				<div className='df'>
+				< TbHelp/>
+				<div className='textdark'>Help and first steps</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Sidebar
---
+++ src/Data/data.js
@@ -0,0 +1,102 @@
+export const datas = [{
+	brand : 'Wfx',
+	descr : 'Develop a personalized',
+	member :'',
+	category : "Automation",
+	tags: '#DigitalTransformation',
+	next:'in 30 min',
+	id:1,
+	img:"wfx.png",
+	img2:"6.png"
+},
+{
+	brand : 'Shopify',
+	descr : 'Develop a personalized',
+	member :'',
+	category : "E-commerce",
+	tags: '#onlineshopingtion',
+	next:'in 50 min',
+	id:2,
+	img:"shopify.png",
+	img2:"3.png"
+
+
+},
+{
+	brand : 'Paypal',
+	descr : 'Develop a personalized',
+	member :'',
+	category : "Automation",
+	tags: '#Businespartnership',
+	next:'in 6 hrs',
+	id:3,
+	img:"paypal.png",
+	img2:"6.png"
+
+
+},
+{
+	brand : 'Disney',
+	descr : 'Develop a personalized',
+	member :'',
+	category : "Technology Saas",
+	tags: '#Logitech',
+	next:'in 30 min',
+	id:4,
+	img:"disney.png",
+	img2:"6.png"
+
+
+},
+{
+	brand : 'Mailchirp',
+	descr : 'Develop a personalized',
+	member :'',
+	category : "Automation",
+	tags: '#DigitalTransformation',
+	next:'in 30 min',
+	id:5,
+	img:"mail.png",
+	img2:"3.png"
+
+
+},
+{
+	brand : 'Google',
+	descr : 'Develop a personalized',
+	member :'',
+	category : "Automation",
+	tags: '#Cyclic',
+	next:'in 1 day',
+	id:6,
+	img:"google.png",
+	img2:"3.png"
+
+
+},
+{
+	brand : 'Microsoft',
+	descr : 'Develop a personalized',
+	member :'',
+	category : "Finance",
+	tags: '#SmartFinance',
+	next:'in 20 min',
+	id:7,
+	img:"micro.png",
+	img2:"6.png"
+
+
+},
+{
+	brand : 'Invision',
+	descr : 'Develop a personalized',
+	member :'',
+	category : "Automation",
+	tags: '#DigitalTransformation',
+	next:'in 30 min',
+	id:8,
+	img:"invisin.png",
+	img2:"6.png"
+
+
+}]
